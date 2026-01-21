9c80c3c155c76008795ad689e7ac959d3fdc0995
Minh Huy Luong <minhhuy112ishere@gmail.com>
2026-01-21 14:45:45 +0000

    feat: add docker-compose setup

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,5 +1,5 @@
 
-
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -28,43 +28,40 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
-
-    year = 2021
-    month = 1 
-    pg_user = 'root'
-    pg_password = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_database = 'ny_taxi'
-    chunksize=100000
-    target_table = 'yellow_taxi_data'
-
+@click.command()
+@click.option('--year', default=2021, type=int, help='Year of data')
+@click.option('--month', default=1, type=int, help='Month of data')
+@click.option('--user', default='root', help='PostgreSQL user')
+@click.option('--password', default='root', help='PostgreSQL password')
+@click.option('--host', default='localhost', help='PostgreSQL host')
+@click.option('--port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--table', default='yellow_taxi_data', help='Target table name')
+def ingest_data(year, month, user, password, host, port, db, table):
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
-    engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
-
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
     df_iter = pd.read_csv(
         url,
         dtype=dtype,
         parse_dates=parse_dates,
         iterator=True,
-        chunksize=chunksize
+        chunksize = 100000
     )
     first = True
     for df_chunk in tqdm(df_iter):
         if first:
             df_chunk.head(0).to_sql(
-                name=target_table, 
+                name=table, 
                 con=engine, 
                 if_exists='replace'
             )
             first = False
         df_chunk.to_sql(
-            name=target_table, 
+            name=table, 
             con=engine, 
             if_exists='append'
         )
 
 if __name__ == '__main__':
-    run()
+    ingest_data()
